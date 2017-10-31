b4540de0868a99bf1100779efcdaf1bed6cd6890
Slava Peshko <slava.peshko@gmail.com>
2017-10-31 18:20:51 +0300

    +added files for GeekQuiz project

---
+++ GeekQuiz/GeekQuiz/Controllers/TriviaController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using GeekQuiz.Models;
+
+namespace GeekQuiz.Controllers
+{
+    public class TriviaController : ApiController
+    {
+		private TriviaContext _context = new TriviaContext();
+
+	    // GET api/Trivia
+		public async Task<IHttpActionResult> Get()
+	    {
+		    var userId = User.Identity.Name;
+
+		    TriviaQuestion nextQuestion = await this.NextQuestionAsync(userId);
+
+		    if (nextQuestion == null)
+		    {
+			    return this.NotFound();
+		    }
+
+		    return this.Ok(nextQuestion);
+	    }
+
+	    [ResponseType(typeof(TriviaAnswer))]
+	    public async Task<IHttpActionResult> Post(TriviaAnswer answer)
+	    {
+		    if (!ModelState.IsValid)
+		    {
+			    return this.BadRequest(this.ModelState);
+		    }
+
+		    answer.UserId = User.Identity.Name;
+
+		    var isCorrect = await this.StoreAsync(answer);
+		    return this.Ok<bool>(isCorrect);
+	    }
+
+		protected override void Dispose(bool disposing)
+	    {
+		    if (disposing)
+		    {
+			    this._context.Dispose();
+		    }
+			base.Dispose(disposing);
+	    }
+
+		private async Task<TriviaQuestion> NextQuestionAsync(string userId)
+		{
+			var lastQuestionId = await this._context.TriviaAnswers
+				.Where(a => a.UserId == userId)
+				.GroupBy(a => a.QuestionId)
+				.Select(g => new { QuestionId = g.Key, Count = g.Count() })
+				.OrderByDescending(q => new { q.Count, QuestionId = q.QuestionId })
+				.Select(q => q.QuestionId)
+				.FirstOrDefaultAsync();
+
+			var questionsCount = await this._context.TriviaQuestions.CountAsync();
+
+			var nextQuestionId = (lastQuestionId % questionsCount) + 1;
+			return await this._context.TriviaQuestions.FindAsync(CancellationToken.None, nextQuestionId);
+		}
+
+		private async Task<bool> StoreAsync(TriviaAnswer answer)
+	    {
+		    this._context.TriviaAnswers.Add(answer);
+
+		    await this._context.SaveChangesAsync();
+		    var selectedOption = await this._context.TriviaOptions
+			    .FirstOrDefaultAsync(n => n.Id == answer.QuestionId);
+
+		    return selectedOption.IsCorrect;
+	    }
+    }
+}
---
+++ GeekQuiz/GeekQuiz/Scripts/app/quiz-controller.js
@@ -0,0 +1,60 @@
+﻿angular.module("QuizApp", [])
+	.controller("QuizCtrl",
+		function ($scope, $http) {
+			$scope.answered = false;
+			$scope.title = "loading question...";
+			$scope.options = [];
+			$scope.correctAnswer = false;
+			$scope.working = false;
+
+			$scope.answer = function () {
+				return $scope.correctAnswer ? 'correct' : 'incorrect';
+			};
+
+			$scope.nextQuestion = function () {
+				$scope.working = true;
+				$scope.answered = false;
+				$scope.title = "loading question...";
+				$scope.options = [];
+
+				$http({
+					method: 'GET', url: "/api/trivia"
+				}).then(function (response) {
+
+					var data = response.data;
+
+					$scope.options = data.options;
+					$scope.title = data.title;
+					$scope.answered = false;
+					$scope.working = false;
+
+				}, function (response) {
+
+					var data = response.data;
+
+					$scope.title = "Oops... something went wrong";
+					$scope.working = false;
+				});
+			};
+
+			$scope.sendAnswer = function (option) {
+				$scope.working = true;
+				$scope.answered = true;
+
+				$http({
+					method: 'POST',
+					url: '/api/trivia',
+					data: { 'questionId': option.questionId, 'optionId': option.id }
+				}).then(function (response) {
+
+					var data = response.data;
+
+					$scope.correctAnswer = (data === true);
+					$scope.working = false;
+				}, function (response) {
+
+					$scope.title = "Oops... something went wrong";
+					$scope.working = false;
+				});
+			};
+		});
